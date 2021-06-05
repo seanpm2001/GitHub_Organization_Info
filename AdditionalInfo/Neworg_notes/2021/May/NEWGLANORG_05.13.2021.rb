528f080653e175a8891d4b3cca6ec04217a607b0
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-04 17:14:21 -0700

    2 new note files - June 4th 2021
    2 new note files from May 13th 2021, now ready for upload. Both files are in Ruby format.

---
+++ AdditionalInfo/Neworg_notes/2021/May/NEWGLANORG_05.13.2021.rb
@@ -0,0 +1,108 @@
+Vue
+Verilog
+SystemVerilog
+CommonLisp
+EmacsLisp
+PureScript
+Isabelle
+Modula-3
+Modula-2
+XSLT
+QML
+Less
+SCSS
+Sass
+Stylus
+Handlebars
+Raku
+Limbo
+Julia
+Smalltalk
+Yacc
+Ada
+ASP DOT NET
+Action Server Pages
+B
+Eiffel
+BASIC
+ABC
+Dart
+CAML
+Lua # (spell it out)
+Clojure
+Motorola S-Record
+SGML # (spell it out)
+YAML
+F#
+XML
+JScript DOT NET
+Visual Basic DOT NET
+CSound
+Diff
+Jython
+JScript
+IronPython
+IntelHexBinary
+Markdown
+MediaWiki
+MS-DOS Style
+PowerShell
+Blockly
+ReStructured Text
+Scratch
+VHDL # (spell it out)
+SXML
+TCL
+VerilogAMS
+WebAssembly
+JupyterNotebook
+JSSS
+Seed7
+Oberon
+NVidia Cuda
+TOML
+Modelica
+VimScript
+Inform
+
+# SRC: https://github.com/SNU-Programming-Tools
+
+puts "| {
+seanpm2001-fsharp-lang
+seanpm2001-objective-c-lang
+seanpm2001-swift-lang
+seanpm2001-scala-lang
+seanpm2001-css-lang
+seanpm2001-d-lang
+seanpm2001-lispflavorederlang-lang
+seanpm2001-r-lang
+seanpm2001-rebol-lang
+seanpm2001-scheme-lang
+} |"
+
+# May 15th 2021
+
+puts "| {
+seanpm2001-objective-j-lang
+seanpm2001-sql-lang
+seanpm2001-piglatin-lang
+seanpm2001-csharp-lang
+seanpm2001-algol-lang
+seanpm2001-hack-lang
+seanpm2001-julia-lang
+seanpm2001-awk-lang
+seanpm2001-m4-lang
+seanpm2001-glsl-lang
+} |"
+
+# May 16th 2021
+
+puts "| {
+seanpm2001-actionscript-lang
+seanpm2001-powershell-lang
+seanpm2001-brightscript-lang
+seanpm2001-crystal-lang
+seanpm2001-fortran-lang
+seanpm2001-liquid-lang
+| }"
+
