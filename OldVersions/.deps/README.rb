1b1464603a4babf77f13368be6a2879e5d3e83b7
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-09-27 19:56:25 -0700

    README.rb
    README.rb

---
+++ OldVersions/.deps/README.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+# Start of script
+# Dependencies for: seanpm2001/GitHub_Organization_Info
+# Ruby (version 2.2 or later)
+# CSV (Comma Separated Values) (any version)
+# Markdown (CommonMark)
+# HTML5 (HTML 5.3)
+# YAML (v1.1)
+# GNU Make (version 1.0 or later)
+# ReStructured Text (version 1.0 or later)
+# WikiText (version 2.0 or later)
+# Citation file (CFF version 1.2 or later)
+# SVG (version 1.1 or later)
+# Plain text version (Any)
+
+echo "Dependencies for: seanpm2001/GitHub_Organization_Info"
+echo "Ruby (version 2.2 or later)"
+echo "CSV (Comma Separated Values) (any version)"
+echo "Markdown (CommonMark)"
+echo "HTML5 (HTML 5.3)"
+echo "YAML (v1.1)"
+echo "GNU Make (version 1.0 or later)"
+echo "ReStructured Text (version 1.0 or later)"
+echo "WikiText (version 2.0 or later)"
+echo "Citation file (CFF version 1.2 or later)"
+echo "SVG (version 1.1 or later)"
+echo "Plain text version (Any)"
+
+break
+
+# File info
+# File type: Ruby source file
+# File version: 2 (2022, Monday, March 7th at 8:40 pm)
+# Line count (including blank lines and compiler line): 36
+# End of script
